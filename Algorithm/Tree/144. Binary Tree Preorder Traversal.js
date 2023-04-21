0adcf37b62858437db19d27e41560c509f5e3b9f
Yifan Lou <lyf496357313@gmail.com>
2023-04-21 08:51:57 +0800

    feat: 144 - Binary Tree Preorder Traversal.js

---
+++ Algorithm/Tree/144. Binary Tree Preorder Traversal.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  let res = [];
+
+  preorder(root);
+  return res;
+
+  function preorder(root) {
+    if (root === null) return;
+    res.push(root.val);
+    preorder(root.left);
+    preorder(root.right);
+  }
+};
