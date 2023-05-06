beee85333ba0fcaaacef106107e6a4fabc793134
Yifan Lou <lyf496357313@gmail.com>
2023-05-06 08:25:58 +0800

    feat: 104 - Maximum Depth of Binary Tree

--- Algorithm/Tree/104. Maximum Depth of Binary Tree.js
+++ Algorithm/Tree/104. Maximum Depth of Binary Tree.js
@@ -30,3 +30,9 @@ var maxDepth = function (root) {
   }
   return max;
 };
+
+// 法二:
+var maxDepth = function (root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
